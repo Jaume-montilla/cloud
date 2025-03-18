55a4a34797bb97715937942e3b3c078e84e504af
Jaume Montilla <jaume070903@gmail.com>
2025-03-18 07:58:39 +0000

    some chgs

--- backend/users/funcionara/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    assert_eq!(hello_package::hosting::seat_at_table(), "sit down please");
-    assert_eq!(hello_package::eat_at_restaurant(),"yummy yummy!");
-    let a: i32 = 123;
-}
